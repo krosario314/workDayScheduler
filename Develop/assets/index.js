a276adbec3ca3c942f6e9ad4c1ed885f3cf51842
Kayla Rosario <rosariok314@gmail.com>
2022-06-22 21:18:57 -0400

    added variables to javascript

--- Develop/assets/index.js
+++ Develop/assets/index.js
@@ -1,2 +1,14 @@
-// Display date and time of users location
-var todaysDate = moment().format("dddd, MMM Do YYYY")
+// Variables
+// Display current date and time of users location
+const currentDate = moment().format("dddd, MMM Do YYYY");
+$("currentDay").html(currentDate);
+const saveBtn = $(".saveBtn");
+
+// Make save button functional
+$(document.ready(function(){
+    $(".saveBtn").on("click", function(){
+        // More variables
+        const time = $(this).parent().attr("id");
+        const text = $(this).siblings(".description").val();
+        localStorage.setItem(time, text);
+    })
