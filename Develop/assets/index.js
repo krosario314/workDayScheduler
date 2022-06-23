de28a1db94bf11f6752160ac4ac776e8cd4f829c
Kayla Rosario <rosariok314@gmail.com>
2022-06-22 21:22:19 -0400

    added color to hour blocks, functional save button

--- Develop/assets/index.js
+++ Develop/assets/index.js
@@ -11,4 +11,33 @@ $(document.ready(function(){
         const time = $(this).parent().attr("id");
         const text = $(this).siblings(".description").val();
         localStorage.setItem(time, text);
-    });
+    })
+// Adding color to hour blocks
+    function scheduleBlockColor() {
+        const hour = moment().hour();
+        $(".time-block").each(function(){
+            const currentHour = parseInt($(this).attr("id"));
+            if (currentHour > hour){
+                $(this).addClass("future");
+            } else if (currentHour === hour) {
+                $(this).addClass("present");
+            } else {
+                $(this).addClass("past");
+            }
+        })
+    };
+// Make specific hour block save button functional
+saveBtn.on("click", function(){
+    const time = $(this).siblings(".hour").text();
+    const plan = $(this).siblings(".plan").val();
+    // saving in localStorage
+    localStorage.setItem(time, plan);
+});
+// Saves all events
+function saveEvent(){
+    $(".hour").each(function(){
+        const currentHour = $(this).text();
+        const currentEvent = localStorage.getItem(currentHour);
+    })
+}
+}))
